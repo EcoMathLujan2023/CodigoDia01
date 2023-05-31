30a0bf37eba142ba98574740b6d7652568619abb
Leonardo Saravia <lsaravia@ungs.edu.ar>
2023-05-31 19:00:48 -0300

    Proyecto y archivo

---
+++ experimento01.jl
@@ -0,0 +1,8 @@
+
+# Arrancar el interprete de Julia ctrl-shift-p   start REPL
+
+# ir al administrador de paquetes ] activar el proyecto
+
+# agregar el paquete Plots add Plots
+
+# ir a la seccion de github que tiene un 2
